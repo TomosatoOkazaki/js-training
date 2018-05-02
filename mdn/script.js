003ea8f54888bb530a69b56a0e5776e0e995814a
TomosatoOkazaki <okazaki@logiclogic.jp>
2018-05-02 02:56:55 +0000

    step1 done.

---
+++ mdn/script.js
@@ -0,0 +1,14 @@
+
+var para = document.querySelector('p');
+
+function createParagraph(){
+  var para = document.createElement('p');
+  para.textContent = 'ボタンが押されました！' ;
+  document.body.appendChild(para);
+}
+
+var buttonsp= document.querySelectorAll('button');
+
+for(var i = 0;i<buttons.length;i++){
+  buttons[i].addEventListener('click',createParagraph);
+}
